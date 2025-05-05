66ec392644457a8e564adc20ae3ee3e6a00151e2
Wesley Finck <wesleyfinck@gmail.com>
2025-05-05 15:53:14 -0700

    define a new interface for publishing multiple annotations at once

---
+++ src/modules/annotations/application/ports/IAnnotationsFromTemplatePublisher.ts
@@ -0,0 +1,25 @@
+import { Result } from "../../../../shared/core/Result";
+import { UseCaseError } from "../../../../shared/core/UseCaseError";
+import { AnnotationsFromTemplate } from "../../domain/aggregates/AnnotationsFromTemplate";
+import { AnnotationId } from "../../domain/value-objects";
+import { PublishedRecordId } from "../../domain/value-objects/PublishedRecordId";
+
+export type PublishedAnnotationsFromTemplateResult = Map<
+  AnnotationId,
+  PublishedRecordId
+>;
+
+export interface IAnnotationsFromTemplatePublisher {
+  publish(
+    annotationsFromTemplate: AnnotationsFromTemplate
+  ): Promise<Result<PublishedAnnotationsFromTemplateResult, UseCaseError>>;
+
+  /**
+   * Unpublishes (deletes) an Annotation record.
+   * @param recordId The ID (e.g., AT URI) of the record to unpublish.
+   * @returns A Result indicating success (void) or a UseCaseError on failure.
+   */
+  unpublish(
+    recordIds: PublishedRecordId[]
+  ): Promise<Result<void, UseCaseError>>;
+}

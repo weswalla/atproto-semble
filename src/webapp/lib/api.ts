aa69b62142fe7cf978da000c213606144340cf57
Wesley Finck <wesleyfinck@gmail.com>
2025-05-14 16:01:55 -0700

    removed unused pages

--- src/webapp/lib/api.ts
+++
@@ -1,6 +0,0 @@
-// Keeping it temporarily for backward compatibility
-// TODO: Remove this file once all imports are updated
-
-export const getApiBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001';
-};

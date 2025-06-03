120f1f8d5ffa0942ed2ae38d980edd68b2c34f03
Wesley Finck <wesleyfinck@gmail.com>
2025-06-03 16:39:05 -0700

    format card

--- src/modules/cards/domain/Card.ts
+++ src/modules/cards/domain/Card.ts
@@ -3,13 +3,13 @@ import { UniqueEntityID } from "../../../shared/domain/UniqueEntityID";
 import { ok, err, Result } from "../../../shared/core/Result";
 import { CardId } from "./value-objects/CardId";
 import { CardType, CardTypeEnum } from "./value-objects/CardType";
-import { CardContent, CardContentValidationError } from "./value-objects/CardContent";
+import { CardContent } from "./value-objects/CardContent";
 import { CuratorId } from "../../annotations/domain/value-objects/CuratorId";
 
 export class CardValidationError extends Error {
   constructor(message: string) {
     super(message);
-    this.name = 'CardValidationError';
+    this.name = "CardValidationError";
   }
 }
 
@@ -70,11 +70,16 @@ export class Card extends AggregateRoot<CardProps> {
   }
 
   get isStandaloneNote(): boolean {
-    return this.isNoteCard && !this.props.parentCardId && !this.props.sourceCardId;
+    return (
+      this.isNoteCard && !this.props.parentCardId && !this.props.sourceCardId
+    );
   }
 
   get isLinkedNote(): boolean {
-    return this.isNoteCard && (!!this.props.parentCardId || !!this.props.sourceCardId);
+    return (
+      this.isNoteCard &&
+      (!!this.props.parentCardId || !!this.props.sourceCardId)
+    );
   }
 
   private constructor(props: CardProps, id?: UniqueEntityID) {
@@ -82,7 +87,7 @@ export class Card extends AggregateRoot<CardProps> {
   }
 
   public static create(
-    props: Omit<CardProps, 'createdAt' | 'updatedAt'>, 
+    props: Omit<CardProps, "createdAt" | "updatedAt">,
     id?: UniqueEntityID
   ): Result<Card, CardValidationError> {
     // Validate content type matches card type
@@ -100,36 +105,55 @@ export class Card extends AggregateRoot<CardProps> {
     const cardProps: CardProps = {
       ...props,
       createdAt: now,
-      updatedAt: now
+      updatedAt: now,
     };
 
     return ok(new Card(cardProps, id));
   }
 
   private static validateCardRelationships(
-    props: Omit<CardProps, 'createdAt' | 'updatedAt'>
+    props: Omit<CardProps, "createdAt" | "updatedAt">
   ): Result<void, CardValidationError> {
     // URL cards should not have parent or source cards
-    if (props.type.value === CardTypeEnum.URL && (props.parentCardId || props.sourceCardId)) {
-      return err(new CardValidationError("URL cards cannot have parent or source cards"));
+    if (
+      props.type.value === CardTypeEnum.URL &&
+      (props.parentCardId || props.sourceCardId)
+    ) {
+      return err(
+        new CardValidationError("URL cards cannot have parent or source cards")
+      );
     }
 
     // HIGHLIGHT cards should have a source card (the content being highlighted)
     if (props.type.value === CardTypeEnum.HIGHLIGHT && !props.sourceCardId) {
-      return err(new CardValidationError("Highlight cards must have a source card"));
+      return err(
+        new CardValidationError("Highlight cards must have a source card")
+      );
     }
 
     // NOTE cards can have either parentCardId OR sourceCardId, but not both
-    if (props.type.value === CardTypeEnum.NOTE && props.parentCardId && props.sourceCardId) {
-      return err(new CardValidationError("Note cards cannot have both parent and source cards"));
+    if (
+      props.type.value === CardTypeEnum.NOTE &&
+      props.parentCardId &&
+      props.sourceCardId
+    ) {
+      return err(
+        new CardValidationError(
+          "Note cards cannot have both parent and source cards"
+        )
+      );
     }
 
     return ok(undefined);
   }
 
-  public updateContent(newContent: CardContent): Result<void, CardValidationError> {
+  public updateContent(
+    newContent: CardContent
+  ): Result<void, CardValidationError> {
     if (this.props.type.value !== newContent.type) {
-      return err(new CardValidationError("Cannot change card content to different type"));
+      return err(
+        new CardValidationError("Cannot change card content to different type")
+      );
     }
 
     this.props.content = newContent;

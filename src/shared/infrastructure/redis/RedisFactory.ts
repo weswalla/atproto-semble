48ab6a04fece1810dac3c767ec40cf3c84aa9db6
Wesley Finck <wesleyfinck@gmail.com>
2025-07-31 13:33:16 -0500

    include max retried config for redis

--- src/shared/infrastructure/redis/RedisFactory.ts
+++ src/shared/infrastructure/redis/RedisFactory.ts
@@ -4,7 +4,7 @@ export class RedisFactory {
   private static instance: Redis | null = null;
   static createConnection(redisUrl: string): Redis {
     if (!this.instance) {
-      this.instance = new Redis(redisUrl);
+      this.instance = new Redis(redisUrl, { maxRetriesPerRequest: null });
     }
     return this.instance;
   }

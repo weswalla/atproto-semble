50dfe808534fe8087c5305d3471510259b476d13
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-08 11:05:27 -0700

    chore: update metadata and cosmik logo

--- src/webapp/app/layout.tsx
+++ src/webapp/app/layout.tsx
@@ -11,8 +11,8 @@ import { theme } from '@/styles/theme';
 import { Hanken_Grotesk } from 'next/font/google';
 
 export const metadata: Metadata = {
-  title: 'Semble',
-  description: 'A social knowledge tool for researchers',
+  title: 'Semble | A social knowledge tool for researchers',
+  description: `Follow your peers' research trails. Surface and discover new connections. Built on ATProto so you own your data.`,
 };
 
 const hankenGrotesk = Hanken_Grotesk({

02b54b7f28bb6833dae6c773ff0d237e6985b39b
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-20 11:59:19 -0700

    feat: app layout

--- src/webapp/app/(authenticated)/layout.tsx
+++ src/webapp/app/(authenticated)/layout.tsx
@@ -3,19 +3,14 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/hooks/useAuth';
-import { useDisclosure } from '@mantine/hooks';
-import { ActionIcon, AppShell, Affix } from '@mantine/core';
-import { FiPlus } from 'react-icons/fi';
-import Header from '@/components/navigation/header/Header';
-import Navbar from '@/components/navigation/navbar/Navbar';
+import AppLayout from '@/components/navigation/appLayout/AppLayout';
 
 interface Props {
   children: React.ReactNode;
 }
-export default function AuthenticatedLayout(props: Props) {
+export default function Layout(props: Props) {
   const router = useRouter();
   const { isAuthenticated, isLoading } = useAuth();
-  const [opened, { toggle }] = useDisclosure();
 
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
@@ -27,32 +22,5 @@ export default function AuthenticatedLayout(props: Props) {
     return null; // Redirecting
   }
 
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened, desktop: opened },
-      }}
-      padding="md"
-    >
-      <Header onToggleNavbar={toggle} />
-      <Navbar />
-
-      <AppShell.Main>
-        {props.children}
-        <Affix position={{ bottom: 20, right: 20 }}>
-          <ActionIcon
-            onClick={() => router.push('/cards/add')}
-            size={'input-lg'}
-            radius="xl"
-            variant="filled"
-          >
-            <FiPlus size={26} />
-          </ActionIcon>
-        </Affix>
-      </AppShell.Main>
-    </AppShell>
-  );
+  return <AppLayout>{props.children}</AppLayout>;
 }

4602313ebd3dddf16e58c33af04e5c9fbc4980bf
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-20 16:09:58 -0700

    fix: remove unused import

--- src/webapp/components/navigation/appLayout/AppLayout.tsx
+++ src/webapp/components/navigation/appLayout/AppLayout.tsx
@@ -2,7 +2,6 @@ import { useDisclosure } from '@mantine/hooks';
 import { AppShell } from '@mantine/core';
 import Header from '@/components/navigation/header/Header';
 import Navbar from '@/components/navigation/navbar/Navbar';
-import { useRouter } from 'next/navigation';
 import ComposerDrawer from '@/features/composer/components/composerDrawer/ComposerDrawer';
 
 interface Props {
@@ -11,7 +10,6 @@ interface Props {
 
 export default function AppLayout(props: Props) {
   const [opened, { toggle }] = useDisclosure();
-  const router = useRouter();
 
   return (
     <AppShell

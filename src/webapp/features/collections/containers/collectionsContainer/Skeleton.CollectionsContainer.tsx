4b5e064cf3d75b26f1e2e5595754339ae52eb8e7
Pouria Delfanazari <pdelfan@gmail.com>
2025-09-09 11:04:03 -0700

    feat: collections container skeleton

--- src/webapp/features/collections/containers/collectionsContainer/Skeleton.CollectionsContainer.tsx
+++ src/webapp/features/collections/containers/collectionsContainer/Skeleton.CollectionsContainer.tsx
@@ -1,5 +1,18 @@
-import { Loader } from '@mantine/core';
+import { Container, SimpleGrid, Stack, Title } from '@mantine/core';
+import CollectionCardSkeleton from '../../components/collectionCard/Skeleton.CollectionCard';
 
 export default function CollectionsContainerSkeleton() {
-  return <Loader />;
+  return (
+    <Container p="xs" size="xl">
+      <Stack>
+        <Title order={1}>Collections</Title>
+
+        <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }} spacing="md">
+          {Array.from({ length: 4 }).map((_, i) => (
+            <CollectionCardSkeleton key={i} />
+          ))}
+        </SimpleGrid>
+      </Stack>
+    </Container>
+  );
 }

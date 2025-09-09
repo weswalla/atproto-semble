86e6c293756d9e366abecb06cee297decb080a47
Pouria Delfanazari <pdelfan@gmail.com>
2025-09-09 09:17:43 -0700

    fix: overlay z-index

--- src/webapp/styles/overlays.ts
+++ src/webapp/styles/overlays.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_OVERLAY_PROPS = {
   inset: 0,
-  zIndex: 101,
+  zIndex: 102,
   blur: 3,
   gradient:
     'linear-gradient(0deg, rgba(204, 255, 0, 0.5), rgba(255, 255, 255, 0.5))',

caf0b8883192b9effba21e1388e4a0bf50c644d8
Pouria Delfanazari <pdelfan@gmail.com>
2025-09-09 11:18:10 -0700

    feat: collection container skeleton

--- src/webapp/features/collections/containers/collectionContainer/Skeleton.CollectionContainer.tsx
+++ src/webapp/features/collections/containers/collectionContainer/Skeleton.CollectionContainer.tsx
@@ -1,5 +1,48 @@
-import { Loader } from '@mantine/core';
+import UrlCardSkeleton from '@/features/cards/components/urlCard/Skeleton.UrlCard';
+import {
+  Container,
+  Grid,
+  GridCol,
+  Group,
+  Skeleton,
+  Stack,
+  Text,
+} from '@mantine/core';
 
 export default function CollectionContainerSkeleton() {
-  return <Loader />;
+  return (
+    <Container p="xs" size="xl">
+      <Stack justify="flex-start">
+        <Group justify="space-between" align="start">
+          <Stack gap={0}>
+            <Text fw={700} c="grape">
+              Collection
+            </Text>
+            {/* Title */}
+            <Skeleton w={300} h={27} />
+
+            {/* Description */}
+            <Skeleton w={'80%'} h={22} mt={'lg'} />
+          </Stack>
+
+          <Stack>
+            {/* By */}
+            <Skeleton w={100} h={24} />
+          </Stack>
+        </Group>
+
+        <Group justify="end">
+          <Skeleton w={28} h={28} radius={'md'} />
+        </Group>
+
+        <Grid gutter="md">
+          {Array.from({ length: 8 }).map((_, i) => (
+            <GridCol key={i} span={{ base: 12, xs: 6, sm: 4, lg: 3 }}>
+              <UrlCardSkeleton />
+            </GridCol>
+          ))}
+        </Grid>
+      </Stack>
+    </Container>
+  );
 }

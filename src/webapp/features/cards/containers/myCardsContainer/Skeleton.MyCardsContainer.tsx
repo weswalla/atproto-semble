798735ddcf8cfe5f41e9d306841b463f3e3dc17d
Pouria Delfanazari <pdelfan@gmail.com>
2025-09-09 10:58:54 -0700

    feat: my cards container skeleton

--- src/webapp/features/cards/containers/myCardsContainer/Skeleton.MyCardsContainer.tsx
+++ src/webapp/features/cards/containers/myCardsContainer/Skeleton.MyCardsContainer.tsx
@@ -1,5 +1,20 @@
-import { Loader } from '@mantine/core';
+import { Container, Grid, GridCol, Stack, Title } from '@mantine/core';
+import UrlCardSkeleton from '../../components/urlCard/Skeleton.UrlCard';
 
 export default function MyCardsContainerSkeleton() {
-  return <Loader />;
+  return (
+    <Container p="xs" size="xl">
+      <Stack>
+        <Title order={1}>My Cards</Title>
+
+        <Grid gutter="md">
+          {Array.from({ length: 8 }).map((_, i) => (
+            <GridCol key={i} span={{ base: 12, xs: 6, sm: 4, lg: 3 }}>
+              <UrlCardSkeleton />
+            </GridCol>
+          ))}
+        </Grid>
+      </Stack>
+    </Container>
+  );
 }

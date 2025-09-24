e6332e96c0ca65359cb8ae2c84d0b8520171d357
Pouria Delfanazari <pdelfan@gmail.com>
2025-09-24 10:54:09 -0700

    chore: move router logic from tab item to profile tabs

--- src/webapp/features/profile/components/profileTabs/ProfileTabs.tsx
+++ src/webapp/features/profile/components/profileTabs/ProfileTabs.tsx
@@ -2,31 +2,32 @@
 
 import { Tabs } from '@mantine/core';
 import TabItem from './TabItem';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
   handle: string;
 }
 
-export default function ProfileTabs(props: Props) {
+export default function ProfileTabs({ handle }: Props) {
+  const router = useRouter();
   const pathname = usePathname();
-  const currentTab = pathname.split('/').slice(3, 4).join('/') || 'profile';
+  const segment = pathname.split('/')[3];
+  const currentTab = segment || 'profile'; // treat base route as 'profile'
+
+  const handleTabChange = (value: string | null) => {
+    if (!value || value === 'profile') {
+      router.push(`/profile/${handle}`);
+    } else {
+      router.push(`/profile/${handle}/${value}`);
+    }
+  };
 
   return (
-    <Tabs defaultValue={'profile'} value={currentTab}>
+    <Tabs value={currentTab} onChange={handleTabChange}>
       <Tabs.List>
-        <TabItem value="profile" href={`/profile/${props.handle}`}>
-          Profile
-        </TabItem>
-        <TabItem value="cards" href={`/profile/${props.handle}/cards`}>
-          Cards
-        </TabItem>
-        <TabItem
-          value="collections"
-          href={`/profile/${props.handle}/collections`}
-        >
-          Collections
-        </TabItem>
+        <TabItem value="profile">Profile</TabItem>
+        <TabItem value="cards">Cards</TabItem>
+        <TabItem value="collections">Collections</TabItem>
       </Tabs.List>
     </Tabs>
   );
--- src/webapp/features/profile/components/profileTabs/TabItem.tsx
+++ src/webapp/features/profile/components/profileTabs/TabItem.tsx
@@ -1,24 +1,14 @@
 import { Tabs } from '@mantine/core';
-import { useRouter } from 'next/navigation';
 import classes from './TabItem.module.css';
 
 interface Props {
   value: string;
-  href: string;
   children: string;
 }
 
 export default function TabItem(props: Props) {
-  const router = useRouter();
-
   return (
-    <Tabs.Tab
-      value={props.value}
-      onClick={() => router.push(props.href)}
-      className={classes.tab}
-      color="dark"
-      fw={600}
-    >
+    <Tabs.Tab value={props.value} className={classes.tab} color="dark" fw={600}>
       {props.children}
     </Tabs.Tab>
   );

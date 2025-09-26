320a0fe3d82ea02a335756f8511de52d07049262
Pouria Delfanazari <pdelfan@gmail.com>
2025-09-26 15:16:15 -0700

    feat: collection page open graph card

--- src/webapp/app/(authenticated)/profile/[handle]/(withoutHeader)/collections/[collectionId]/layout.tsx
+++ src/webapp/app/(authenticated)/profile/[handle]/(withoutHeader)/collections/[collectionId]/layout.tsx
@@ -1,11 +1,30 @@
+import { ApiClient } from '@/api-client/ApiClient';
 import Header from '@/components/navigation/header/Header';
+import { createClientTokenManager } from '@/services/auth';
 import type { Metadata } from 'next';
 import { Fragment } from 'react';
 
-export const metadata: Metadata = {
-  title: 'Collection',
-  description: 'Collection',
-};
+interface Props {
+  params: Promise<{ collectionId: string }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { collectionId } = await params;
+
+  const apiClient = new ApiClient(
+    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
+    createClientTokenManager(),
+  );
+
+  const collection = await apiClient.getCollectionPage(collectionId);
+
+  return {
+    title: collection.name,
+    description:
+      collection.description ??
+      `View ${collection.author.name}'s collection on Semble`,
+  };
+}
 
 interface Props {
   children: React.ReactNode;
---
+++ src/webapp/app/(authenticated)/profile/[handle]/(withoutHeader)/collections/[collectionId]/opengraph-image.tsx
@@ -0,0 +1,58 @@
+import { ApiClient } from '@/api-client';
+import { createClientTokenManager } from '@/services/auth';
+import OpenGraphCard from '@/features/openGraph/components/openGraphCard/OpenGraphCard';
+import { truncateText } from '@/lib/utils/text';
+
+interface Props {
+  params: Promise<{ collectionId: string }>;
+}
+
+export const contentType = 'image/png';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export default async function Image(props: Props) {
+  const { collectionId } = await props.params;
+
+  const apiClient = new ApiClient(
+    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
+    createClientTokenManager(),
+  );
+
+  const collection = await apiClient.getCollectionPage(collectionId);
+
+  return await OpenGraphCard({
+    children: (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 10,
+            marginTop: '35px',
+          }}
+        >
+          <p
+            style={{
+              fontSize: '40px',
+              lineHeight: '20px',
+              color: '#e803ff',
+            }}
+          >
+            Collection
+          </p>
+          <p
+            style={{
+              fontSize: '64px',
+              lineHeight: '20px',
+            }}
+          >
+            {truncateText(truncateText(collection.name), 35)}
+          </p>
+        </div>
+      </div>
+    ),
+  });
+}

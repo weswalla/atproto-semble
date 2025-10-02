c4d3656b0d30dbad0c460ed37da5dcdd533c2f6f
Pouria Delfanazari <pdelfan@gmail.com>
2025-10-02 15:46:21 -0700

    docs: note card story

---
+++ src/webapp/features/notes/components/noteCard/NoteCard.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import NoteCard from './NoteCard';
+
+const meta = {
+  component: NoteCard,
+} satisfies Meta<typeof NoteCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    note: `This is the note's content`,
+  },
+};

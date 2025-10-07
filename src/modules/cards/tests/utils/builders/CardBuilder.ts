9026ef18bfd2175cb3762a7b2b57c8a6d787a212
Wesley Finck <wesleyfinck@gmail.com>
2025-10-07 16:48:56 -0700

    update card builder

--- src/modules/cards/tests/utils/builders/CardBuilder.ts
+++ src/modules/cards/tests/utils/builders/CardBuilder.ts
@@ -64,7 +64,7 @@ export class CardBuilder {
     return this;
   }
 
-  withOriginalPublishedRecordId(originalPublishedRecordId: {
+  withPublishedRecordId(originalPublishedRecordId: {
     uri: string;
     cid: string;
   }): CardBuilder {

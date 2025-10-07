5d4fa69e294e95cda2be1811ae6583b0a9e47a0b
Wesley Finck <wesleyfinck@gmail.com>
2025-10-07 15:16:50 -0700

    update migration files after schema changes

---
+++ src/shared/infrastructure/database/migrations/0004_brainy_rocket_racer.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "cards" RENAME COLUMN "original_published_record_id" TO "published_record_id";--> statement-breakpoint
+ALTER TABLE "cards" DROP CONSTRAINT "cards_original_published_record_id_published_records_id_fk";
+--> statement-breakpoint
+ALTER TABLE "cards" ADD COLUMN "author_id" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "cards" ADD CONSTRAINT "cards_published_record_id_published_records_id_fk" FOREIGN KEY ("published_record_id") REFERENCES "public"."published_records"("id") ON DELETE no action ON UPDATE no action;

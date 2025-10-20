85048ef65cfbffc5e54b182685fce823aee66f68
Pouria Delfanazari <pdelfan@gmail.com>
2025-10-20 12:13:44 -0700

    feat: improved added by summary

--- src/webapp/features/semble/components/urlAddedBySummary/UrlAddedBySummary.tsx
+++ src/webapp/features/semble/components/urlAddedBySummary/UrlAddedBySummary.tsx
@@ -1,7 +1,44 @@
+import { Avatar, AvatarGroup, Group, Text } from '@mantine/core';
+import { getLibrariesForUrl } from '../../lib/dal';
+import Link from 'next/link';
+
 interface Props {
   url: string;
 }
 
-export default function UrlAddedBySummary(props: Props) {
-  return <>Added by summary</>;
+export default async function UrlAddedBySummary(props: Props) {
+  const data = await getLibrariesForUrl(props.url);
+
+  const MAX_PROFILES_TO_SHOW = 3;
+  const names = data.libraries
+    .slice(0, MAX_PROFILES_TO_SHOW)
+    .map((p) => p.userId);
+  const hasMore = data.libraries.length > MAX_PROFILES_TO_SHOW;
+  const followersSummaryText =
+    'Added by ' + names.join(', ') + (hasMore ? ', and others' : '');
+
+  if (data.libraries.length === 0) {
+    return null;
+  }
+
+  return (
+    <Group gap={'xs'}>
+      {/*<AvatarGroup spacing={'xs'}>
+          {avatars.slice(0, MAX_PROFILES_TO_SHOW).map((p, i) => (
+            <Avatar
+              key={i}
+              component={Link}
+              href={`/dashboard/profile/${p.handle}`}
+              src={p.avatarUrl}
+              alt={`${p.handle}`}
+              name={p.handle}
+              size={30}
+            />
+          ))}
+        </AvatarGroup>*/}
+      <Text fz={'sm'} fw={500} c={'gray'}>
+        {followersSummaryText}
+      </Text>
+    </Group>
+  );
 }

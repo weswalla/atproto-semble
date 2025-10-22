b44f3c8168b755597039a5afba130b94b8f265b2
Pouria Delfanazari <pdelfan@gmail.com>
2025-10-22 11:18:51 -0700

    feat: domain name on semble page og image

--- src/webapp/app/api/opengraph/semble/route.tsx
+++ src/webapp/app/api/opengraph/semble/route.tsx
@@ -10,6 +10,7 @@ interface Metadata {
   title?: string;
   url?: string;
   domain?: string;
+  imageUrl?: string;
 }
 
 export async function GET(request: NextRequest) {
@@ -20,6 +21,7 @@ export async function GET(request: NextRequest) {
   if (url) {
     try {
       const result = await getUrlMetadata(getUrlFromSlug([url]));
+
       metadata = {
         ...(result?.metadata || {}),
         domain: getDomain(url),
@@ -41,27 +43,38 @@ export async function GET(request: NextRequest) {
             marginTop: '35px',
           }}
         >
-          <p
-            style={{
-              fontSize: '40px',
-              lineHeight: '20px',
-              color: '#ff6400',
-            }}
-          >
-            Semble
-          </p>
-          {metadata && (
-            <p
+          {metadata && url && (
+            <div
               style={{
-                fontSize: '64px',
-                lineHeight: '20px',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 10,
               }}
             >
-              {truncateText(
-                metadata.title || metadata.url || metadata.domain || 'Unknown',
-                25,
-              )}
-            </p>
+              <p
+                style={{
+                  fontSize: '40px',
+                  lineHeight: '20px',
+                  color: '#23AFED',
+                }}
+              >
+                {truncateText(metadata.domain || getDomain(url), 35)}
+              </p>
+              <p
+                style={{
+                  fontSize: '64px',
+                  lineHeight: '20px',
+                }}
+              >
+                {truncateText(
+                  metadata.title ||
+                    metadata.url ||
+                    metadata.domain ||
+                    'Unknown',
+                  25,
+                )}
+              </p>
+            </div>
           )}
         </div>
       </div>

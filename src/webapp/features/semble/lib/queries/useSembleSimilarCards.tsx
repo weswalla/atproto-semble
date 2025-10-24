15564660ccbd093ce0c63aa8d6624b7ed033983e
Wesley Finck <wesleyfinck@gmail.com>
2025-10-23 22:15:44 -0700

    refactor: simplify getSimilarUrlsForUrl query parameters

--- src/webapp/features/semble/lib/queries/useSembleSimilarCards.tsx
+++ src/webapp/features/semble/lib/queries/useSembleSimilarCards.tsx
@@ -14,10 +14,10 @@ export default function useSembleSimilarCards(props: Props) {
     queryKey: ['semble similar cards', props.url, limit, props.threshold],
     initialPageParam: 1,
     queryFn: ({ pageParam = 1 }) => {
-      return getSimilarUrlsForUrl(props.url, { 
-        page: pageParam, 
+      return getSimilarUrlsForUrl(props.url, {
+        page: pageParam,
         limit,
-        ...(props.threshold && { threshold: props.threshold })
+        ...(props.threshold && { threshold: props.threshold }),
       });
     },
     getNextPageParam: (lastPage) => {

940f67ebaec133f2d751ac6a2b1f7374415b4715
Pouria Delfanazari <pdelfan@gmail.com>
2025-10-27 16:21:28 -0700

    refactor: useAuth

--- src/webapp/features/collections/lib/queries/useMyCollections.tsx
+++ src/webapp/features/collections/lib/queries/useMyCollections.tsx
@@ -1,19 +1,11 @@
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { getMyCollections } from '../dal';
-import { useAuth } from '@/hooks/useAuth';
 
 interface Props {
   limit?: number;
 }
 
 export default function useMyCollections(props?: Props) {
-  const { isAuthenticated } = useAuth();
-
-  if (!isAuthenticated) {
-    // don't trigger Suspense
-    return { data: null };
-  }
-
   const limit = props?.limit ?? 15;
 
   return useSuspenseInfiniteQuery({

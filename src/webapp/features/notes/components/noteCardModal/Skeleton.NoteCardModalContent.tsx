271a94909099eb41b329323a98b3a4a3c2f66a48
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-09 21:28:41 -0800

    feat: note modal content skeleton design

--- src/webapp/features/notes/components/noteCardModal/Skeleton.NoteCardModalContent.tsx
+++ src/webapp/features/notes/components/noteCardModal/Skeleton.NoteCardModalContent.tsx
@@ -4,13 +4,14 @@ export default function NoteCardModalContentSkeleton() {
   return (
     <Stack gap={'xs'}>
       <Group gap={5}>
-        <Avatar size={'md'} />
+        <Avatar size={'sm'} />
         <Skeleton w={100} h={14} />
       </Group>
       {/* Note */}
       <Skeleton w={'100%'} h={14} />
       <Skeleton w={'100%'} h={14} />
-      <Skeleton w={'100%'} h={14} />
+
+      <Skeleton w={'100%'} h={72} radius={'lg'} />
     </Stack>
   );
 }

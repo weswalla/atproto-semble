c81bcfada10c6157034a2d34605843201af644e1
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-12 15:28:45 -0800

    feat: at uri as alternate links

--- src/webapp/app/(dashboard)/profile/[handle]/(withHeader)/layout.tsx
+++ src/webapp/app/(dashboard)/profile/[handle]/(withHeader)/layout.tsx
@@ -30,6 +30,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         url: `${process.env.APP_URL}/profile/${handle}`,
       },
     ],
+    alternates: {
+      types: {
+        'atprotocol:creator': `at://${profile.id}`,
+      },
+    },
     other: {
       'atprotocol:creator': `at://${profile.id}`,
     },
--- src/webapp/app/(dashboard)/profile/[handle]/(withoutHeader)/collections/[rkey]/layout.tsx
+++ src/webapp/app/(dashboard)/profile/[handle]/(withoutHeader)/collections/[rkey]/layout.tsx
@@ -29,6 +29,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         url: `${process.env.APP_URL}/profile/${handle}`,
       },
     ],
+    alternates: {
+      types: {
+        'atprotocol:creator': `at://${collection.author.id}`,
+      },
+    },
     other: {
       'atprotocol:creator': `at://${collection.author.id}`,
     },

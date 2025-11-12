71fafda953da017112e4126f904bb5f07bbc0114
Wesley Finck <wesleyfinck@gmail.com>
2025-11-12 11:19:19 -0500

    formatting

--- src/webapp/lib/serverFeatureFlags.ts
+++ src/webapp/lib/serverFeatureFlags.ts
@@ -4,7 +4,7 @@ const APPROVED_HANDLES = ['wesleyfinck.org', 'ronentk.me', 'pouriade.com'];
 
 export async function getServerFeatureFlags() {
   const { user } = await getServerAuthStatus();
-  
+
   const isApprovedUser = user?.handle && APPROVED_HANDLES.includes(user.handle);
 
   return {

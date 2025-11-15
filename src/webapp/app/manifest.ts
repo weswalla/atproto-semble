8f8b1919434f97a249a76222d4d86825f6f9d8c2
afterlifepro <vielle.dev@proton.me>
2025-11-15 11:55:54 +0000

    fix: issues with pwa icon presentation

--- src/webapp/app/manifest.ts
+++ src/webapp/app/manifest.ts
@@ -15,11 +15,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/semble-icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/semble-icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };

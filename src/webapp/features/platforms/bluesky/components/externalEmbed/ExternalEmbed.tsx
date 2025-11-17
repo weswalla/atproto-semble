d66f6feb3cd02fafdc924592053a7db7d94bade3
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-17 13:47:09 -0800

    feat: external embed

---
+++ src/webapp/features/platforms/bluesky/components/externalEmbed/ExternalEmbed.tsx
@@ -0,0 +1,37 @@
+import { getDomain } from '@/lib/utils/link';
+import { AppBskyEmbedExternal } from '@atproto/api';
+import {
+  AspectRatio,
+  Card,
+  CardSection,
+  Image,
+  Stack,
+  Text,
+} from '@mantine/core';
+
+interface Props {
+  embed: AppBskyEmbedExternal.View;
+}
+
+export default function ExternalEmbed(props: Props) {
+  return (
+    <Card p={0} withBorder>
+      <CardSection>
+        {props.embed.external.thumb && (
+          <AspectRatio ratio={16 / 9}>
+            <Image src={props.embed.external.thumb} />
+          </AspectRatio>
+        )}
+      </CardSection>
+
+      <Stack gap={0} p={"xs"}>
+        <Text fz={'sm'} fw={500} c={'bright'} lineClamp={1}>
+          {props.embed.external.title}
+        </Text>
+        <Text fz={'sm'} fw={500} c={'gray'} lineClamp={1}>
+          {getDomain(props.embed.external.uri)}
+        </Text>
+      </Stack>
+    </Card>
+  );
+}
--- src/webapp/features/platforms/bluesky/components/postEmbed/PostEmbed.tsx
+++ src/webapp/features/platforms/bluesky/components/postEmbed/PostEmbed.tsx
@@ -1,5 +1,10 @@
-import { AppBskyEmbedImages, AppBskyFeedDefs } from '@atproto/api';
+import {
+  AppBskyEmbedExternal,
+  AppBskyEmbedImages,
+  AppBskyFeedDefs,
+} from '@atproto/api';
 import ImageEmbed from '../imageEmbed/ImageEmbed';
+import ExternalEmbed from '../externalEmbed/ExternalEmbed';
 
 interface Props {
   embed: AppBskyFeedDefs.FeedViewPost['post']['embed'];
@@ -8,7 +13,8 @@ export default function PostEmbed(props: Props) {
   switch (true) {
     case AppBskyEmbedImages.isView(props.embed):
       return <ImageEmbed images={props.embed.images} />;
-
+    case AppBskyEmbedExternal.isView(props.embed):
+      return <ExternalEmbed embed={props.embed} />;
     default:
       return <>default</>;
   }

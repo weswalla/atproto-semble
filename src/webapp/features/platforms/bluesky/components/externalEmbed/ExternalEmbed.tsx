2392b7daa9579f46fda65727dbb1519429427493
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-18 11:28:55 -0800

    feat: compact mode for bluesky external embed

--- src/webapp/features/platforms/bluesky/components/externalEmbed/ExternalEmbed.tsx
+++ src/webapp/features/platforms/bluesky/components/externalEmbed/ExternalEmbed.tsx
@@ -4,6 +4,7 @@ import {
   AspectRatio,
   Card,
   CardSection,
+  Group,
   Image,
   Stack,
   Text,
@@ -11,11 +12,46 @@ import {
 
 interface Props {
   embed: AppBskyEmbedExternal.View;
+  mode?: 'compact' | 'large';
 }
 
 export default function ExternalEmbed(props: Props) {
+  if (props.mode === 'compact') {
+    return (
+      <Card p={'xs'} withBorder>
+        <Group gap={'xs'} wrap="nowrap">
+          {props.embed.external.thumb && (
+            <AspectRatio ratio={1 / 1}>
+              <Image
+                src={props.embed.external.thumb}
+                alt={props.embed.external.description}
+                h={50}
+                w={50}
+                radius={'sm'}
+              />
+            </AspectRatio>
+          )}
+
+          <Stack gap={0}>
+            <Text fz={'sm'} fw={500} c={'bright'} lineClamp={1}>
+              {props.embed.external.title}
+            </Text>
+            <Text fz={'sm'} fw={500} c={'gray'} lineClamp={1}>
+              {getDomain(props.embed.external.uri)}
+            </Text>
+          </Stack>
+        </Group>
+      </Card>
+    );
+  }
   return (
-    <Card p={0} withBorder>
+    <Card
+      p={0}
+      component="a"
+      href={props.embed.external.uri}
+      target="_blank"
+      withBorder
+    >
       <CardSection>
         {props.embed.external.thumb && (
           <AspectRatio ratio={16 / 9}>

a2356dad75203b38d0da62bcc381e3b88c5d528f
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-17 17:08:15 -0800

    feat: video embed

--- src/webapp/features/platforms/bluesky/components/postEmbed/PostEmbed.tsx
+++ src/webapp/features/platforms/bluesky/components/postEmbed/PostEmbed.tsx
@@ -7,6 +7,7 @@ import {
   AppBskyFeedDefs,
   AppBskyGraphDefs,
 } from '@atproto/api';
+
 import ImageEmbed from '../imageEmbed/ImageEmbed';
 import ExternalEmbed from '../externalEmbed/ExternalEmbed';
 import VideoEmbed from '../videoEmbed/VideoEmbed';
@@ -19,38 +20,40 @@ interface Props {
   embed: AppBskyFeedDefs.PostView['embed'];
 }
 
-export default function PostEmbed(props: Props) {
+export default function PostEmbed({ embed }: Props) {
   switch (true) {
-    case AppBskyEmbedImages.isView(props.embed):
-      return <ImageEmbed images={props.embed.images} />;
-    case AppBskyEmbedExternal.isView(props.embed):
-      return <ExternalEmbed embed={props.embed} />;
-    case AppBskyEmbedVideo.isView(props.embed):
-      return <VideoEmbed embed={props.embed} />;
-    case AppBskyEmbedRecord.isView(props.embed): {
-      const record = props.embed.record;
+    case AppBskyEmbedImages.isView(embed):
+      return <ImageEmbed images={embed.images} />;
 
-      if (AppBskyGraphDefs.isListView(record)) {
-        return <ListEmbed list={record} />;
-      }
+    case AppBskyEmbedExternal.isView(embed):
+      return <ExternalEmbed embed={embed} />;
+
+    case AppBskyEmbedVideo.isView(embed):
+      return <VideoEmbed embed={embed} />;
+
+    // Check for Record first before accessing embed.record
+    case AppBskyEmbedRecord.isView(embed): {
+      const record = embed.record;
+
       if (AppBskyGraphDefs.isStarterPackViewBasic(record)) {
         return <StarterPackEmbed embed={record} />;
       }
+
       if (AppBskyFeedDefs.isGeneratorView(record)) {
         return <FeedEmbed feed={record} />;
       }
 
-      if (AppBskyEmbedRecord.isView(props.embed)) {
-        return <RecordEmbed embed={props.embed.record} />;
+      if (AppBskyGraphDefs.isListView(record)) {
+        return <ListEmbed list={record} />;
       }
 
-      if (AppBskyEmbedRecordWithMedia.isView(props.embed)) {
-        return <>record with media</>;
-      }
-
-      break; // fallthrough to default
+      return <RecordEmbed embed={record} />;
     }
-  }
 
-  return <>default</>;
+    case AppBskyEmbedRecordWithMedia.isView(embed):
+      return <RecordEmbed embed={embed.record.record} media={embed.media} />;
+
+    default:
+      return <>default</>;
+  }
 }
--- src/webapp/features/platforms/bluesky/components/recordEmbed/RecordEmbed.tsx
+++ src/webapp/features/platforms/bluesky/components/recordEmbed/RecordEmbed.tsx
@@ -1,11 +1,15 @@
 import RichTextRenderer from '@/components/contentDisplay/richTextRenderer/RichTextRenderer';
-import { AppBskyEmbedRecord, AppBskyFeedPost } from '@atproto/api';
+import {
+  AppBskyEmbedRecord,
+  AppBskyEmbedRecordWithMedia,
+  AppBskyFeedPost,
+} from '@atproto/api';
 import { Stack, Group, Avatar, Box, Text, Card } from '@mantine/core';
-import { record } from 'zod';
 import PostEmbed from '../postEmbed/PostEmbed';
 
 interface Props {
   embed: AppBskyEmbedRecord.View['record'];
+  media?: AppBskyEmbedRecordWithMedia.View['media'];
 }
 
 export default function RecordEmbed(props: Props) {
@@ -20,35 +24,38 @@ export default function RecordEmbed(props: Props) {
   const post = props.embed;
 
   return (
-    <Card p={'sm'} flex={1} h={'100%'} withBorder>
-      <Stack justify="space-between" align="start" gap="sm">
-        <Group gap="xs">
-          <Avatar
-            src={post.author.avatar}
-            alt={`${post.author.handle} social preview image`}
-            radius="xl"
-            size={'sm'}
-          />
-
-          <Stack gap={0} flex={1}>
-            <Text c="bright" lineClamp={1} fw={500} w="fit-content">
-              {post.author.displayName || post.author.handle}
-            </Text>
-            <Text c="gray" lineClamp={1} w="fit-content">
-              @{post.author.handle}
-            </Text>
-          </Stack>
-        </Group>
-        <Stack gap={'xs'}>
-          <Box>
-            <RichTextRenderer
-              text={(post.value as AppBskyFeedPost.Record).text}
-              textProps={{ lineClamp: 1 }}
+    <Stack gap={'xs'}>
+      {props.media && <PostEmbed embed={props.media} />}
+      <Card p={'sm'} flex={1} h={'100%'} withBorder>
+        <Stack justify="space-between" align="start" gap="sm">
+          <Group gap="xs">
+            <Avatar
+              src={post.author.avatar}
+              alt={`${post.author.handle} social preview image`}
+              radius="xl"
+              size={'sm'}
             />
-          </Box>
-          {post.embeds && <PostEmbed embed={post.embeds[0]} />}
+
+            <Stack gap={0} flex={1}>
+              <Text c="bright" lineClamp={1} fw={500} w="fit-content">
+                {post.author.displayName || post.author.handle}
+              </Text>
+              <Text c="gray" lineClamp={1} w="fit-content">
+                @{post.author.handle}
+              </Text>
+            </Stack>
+          </Group>
+          <Stack gap={'xs'}>
+            <Box>
+              <RichTextRenderer
+                text={(post.value as AppBskyFeedPost.Record).text}
+                textProps={{ lineClamp: 1 }}
+              />
+            </Box>
+            {post.embeds && <PostEmbed embed={post.embeds[0]} />}
+          </Stack>
         </Stack>
-      </Stack>
-    </Card>
+      </Card>
+    </Stack>
   );
 }

69cd311ab450fb674cc2c4b7f35a94edfdda43b5
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-19 12:01:33 -0800

    feat: similar cards skeleton grid

--- src/webapp/features/semble/containers/sembleSimilarCardsContainer/Skeleton.SembleSimilarCardsContainer.tsx
+++ src/webapp/features/semble/containers/sembleSimilarCardsContainer/Skeleton.SembleSimilarCardsContainer.tsx
@@ -4,7 +4,7 @@ import { Grid, GridCol, Stack } from '@mantine/core';
 export default function SembleSimilarCardsContainerSkeleton() {
   return (
     <Stack>
-      <Grid gutter="md">
+      <Grid gutter="xl" mx={'auto'} maw={600}>
         {/* not necessary to check if navbar is open */}
         {Array.from({ length: 8 }).map((_, i) => (
           <GridCol key={i} span={12}>

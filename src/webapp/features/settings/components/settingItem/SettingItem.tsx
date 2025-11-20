3939525bc104fc0a9eea98a7482084bef6f482ef
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-20 15:48:29 -0800

    feat: lighter colour on setting item

--- src/webapp/features/settings/components/settingItem/SettingItem.tsx
+++ src/webapp/features/settings/components/settingItem/SettingItem.tsx
@@ -24,6 +24,7 @@ export default function SettingItem(props: Props) {
       size="md"
       justify="start"
       radius={'lg'}
+      c="gray"
       leftSection={renderIcon()}
     >
       {props.children}

4338bc8bbbc086d1774a3f7fe278d00153d198cc
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-20 15:47:15 -0800

    feat: setting item

---
+++ src/webapp/features/settings/components/settingItem/SettingItem.tsx
@@ -0,0 +1,32 @@
+import type { IconType } from 'react-icons/lib';
+import { Button } from '@mantine/core';
+import Link from 'next/link';
+import { isValidElement } from 'react';
+
+interface Props {
+  href: string;
+  icon: IconType | React.ReactElement;
+  children: React.ReactNode;
+}
+export default function SettingItem(props: Props) {
+  const renderIcon = () => {
+    if (isValidElement(props.icon)) return props.icon;
+
+    const IconComponent = props.icon as IconType;
+    return <IconComponent size={22} />;
+  };
+
+  return (
+    <Button
+      component={Link}
+      href={props.href}
+      variant="default"
+      size="md"
+      justify="start"
+      radius={'lg'}
+      leftSection={renderIcon()}
+    >
+      {props.children}
+    </Button>
+  );
+}

7bb3fa2fdbadb34c194700ddda31e12f9810457b
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-21 10:09:55 -0800

    chore: semble default icon

--- src/webapp/app/manifest.ts
+++ src/webapp/app/manifest.ts
@@ -15,6 +15,12 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/semble-icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/semble-icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
         purpose: 'maskable',
       },
       {
